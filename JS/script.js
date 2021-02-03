4fbc15dd778e63bd0a540bb5ea02fafbaccc8794
Adrian Perez <adrianpevelandia@gmail.com>
2021-02-03 15:51:02 +1030

    Improved interface and dark mode included

--- JS/script.js
+++ JS/script.js
@@ -1,38 +1,63 @@
 let numCells = 256;
+let colorMode = "normal"
 drawCanvas(numCells);
 
-const wrapper = document.getElementById('divsContainer');
+const wrapper = document.getElementById('cellsContainer');
 wrapper.addEventListener('mouseover', colourCell);
-
 
 const clearButton = document.getElementById('clearButton');
 clearButton.addEventListener('click', clearCanvas);
 
+const darkModeButton = document.getElementById('darkModeButton');
+darkModeButton.addEventListener('click', setDarkMode);
+
+function setDarkMode() {
+    let cells = document.querySelectorAll('.emptyCellWhite');
+    cells.forEach(element => {
+        element.classList.replace('emptyCellWhite', 'emptyCellBlack');
+    });
+    cells = document.querySelectorAll('.colorCellBlack');
+    cells.forEach(element => {
+        element.classList.replace('colorCellBlack', 'colorCellWhite');
+    });
+    colorMode = "dark";
+}
+
 function drawCanvas(num) {
-    const divsContainer = document.querySelector('#divsContainer');
-    divsContainer.style.setProperty('--numberCells',Math.sqrt(num));
+    const cellsContainer = document.querySelector('#cellsContainer');
+    cellsContainer.style.setProperty('--numberCells', Math.sqrt(num));
     for (let i = 0; i < num; i++) {
         let cellDiv = document.createElement('div');
-        let cellId = document.createAttribute("id");
-        cellDiv.classList.add("cell");
-        divsContainer.appendChild(cellDiv);
+        //let cellId = document.createAttribute("id");
+        cellDiv.classList.add("emptyCellWhite");
+        cellsContainer.appendChild(cellDiv);
     }
 }
 
 function colourCell(e) {
-    if (e.target.parentElement.id == 'divsContainer')
-        e.target.classList.add('change');
+    if (e.target.parentElement.id == 'cellsContainer')
+        switch (colorMode) {
+            case 'normal':
+                e.target.classList.add('colorCellBlack');
+                break;
+            case 'dark':
+                e.target.classList.add('colorCellWhite');
+                break;
+        }
 }
 
 function clearCanvas() {
-    numCells = prompt('Grid Size - Max 100', 16);
-    numCells *= numCells;
-    while (wrapper.firstChild) {
-        wrapper.removeChild(wrapper.lastChild);
+    colorMode = 'normal'
+    numCells = prompt('Grid Size Min 1 - Max 100', 16);
+    if (numCells > 0 && numCells <= 100) {
+        numCells *= numCells;
+        while (wrapper.firstChild) {
+            wrapper.removeChild(wrapper.lastChild);
+        }
+        drawCanvas(numCells)
     }
-    drawCanvas(numCells)
-
+    else {
+        alert("Number of cells is out of range");
+        clearCanvas();
+    }
 }
-
-
-
